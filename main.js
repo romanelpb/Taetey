95c965cd24b79dfea0b266d7c3223c5028e8f8d7
romanelpb <romanebougeoise@gmail.com>
2020-04-24 23:29:27 -0400

    ajout d'une config (token) et de ses commentaires

--- main.js
+++ main.js
@@ -9,21 +9,28 @@ const client = new Discord.Client();
 /*
 Déclenchement de l'évenement «ready».
 */
+/*
+Création de la constance «config» qui effectue la lecture du fichier config.json
+*/
+const config = require('./config.json');
+
 client.on('ready', () => {
 /*
 Envoie d'une log avec le pseudo discord du bot et la confirmation de sa connection au client.
 */
-	console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Connection en tant que ${client.user.tag}!`);
 /*
 Affiche un status de jeu personnalisé
 */
 	client.user.setActivity('!help')
 });
 /*
-Connection du client à l'aide du token
+Connection du client à l'aide du token présent dans la config
 */
-client.login('NzAzMTM5MTA1NDY2MzUxNjQ3.XqOiIg.F3D3h5p0H2t-BLvgB73uZ0J0pLQ');
+client.login(config.token);
 
 // «»
 
 // { }
+
+// 1gpjd2
